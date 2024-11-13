0b5d687caf778f9085d526a2435f99fbd0601640
Rasmus Bäckström <rasmusbaeckstrom@gmail.com>
2024-11-13 09:28:06 +0100

    Initial commit

---
+++ src/main/java/org/example/sqllab2/SqlLab2Application.java
@@ -0,0 +1,13 @@
+package org.example.sqllab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlLab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlLab2Application.class, args);
+	}
+
+}
